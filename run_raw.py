37b574634ee5f86a44d206dbf1d7eca7e85d67a2
Andre Fernando Lara <andrefernandolara@hotmail.com>
2024-02-08 10:48:32 -0300

    feat: Implementing App in src

--- run_raw.py
+++ run_raw.py
@@ -8,10 +8,12 @@ app = Flask(__name__)
 def create_tag():
     body = request.json
     product_code = body.get("product_code")
+
     tag = Code128(product_code, writer=ImageWriter())
     path_from_tag = f'{tag}'
     tag.save(path_from_tag)
 
     return jsonify({ "tag path": path_from_tag })
+
 if __name__  == "__main__":
     app.run(host="0.0.0.0" , port=3000)
